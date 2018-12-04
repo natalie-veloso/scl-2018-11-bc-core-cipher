499819c433510420cef0c3f3c36764b6c3e8ac9d
natalie-veloso <natalie.veloso.z@gmail.com>
2018-12-04 09:55:43 -0300

    Cifrando y Descifrando

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,27 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+  encode: (text, offset) => {
+    let resultadoCifrado = "";
+        for (let i = 0; i <= text.length; i++){ 
+          let textoEntregado = text.toUpperCase();
+          let ascii = textoEntregado.charCodeAt(i)
+          let formula = ((ascii - 65 + parseInt(offset)) % 26) + 65;
+          let nuevoAscii = String.fromCharCode(formula);
+          resultadoCifrado += nuevoAscii;
+
+    }
+    return resultadoCifrado;
+},
+
+  decode: (textB, offsetB) => {
+    let resultadoDescifrado = "";
+        for (let i = 0; i <= textB.length; i++){
+          let textoEntregadoB = textB.toUpperCase();
+          let asciiB = textoEntregadoB.charCodeAt(i)
+          let formulaB = ((asciiB - 90 - parseInt(offsetB)) % 26) + 90;
+          let nuevoAsciiB = String.fromCharCode(formulaB);
+          resultadoDescifrado += nuevoAsciiB;
+
+    }
+    return resultadoDescifrado;
+
+}};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-/* Acá va tu código */
+function cifrar() {
+    let text = document.getElementById("texto").value;
+    let offset = document.getElementById("desplazamiento").value;
+    let resultadoCifrado = cipher.encode(text, offset);
+    document.getElementById("textoCifrado").innerHTML = resultadoCifrado;
+
+}
+
+function descifrar() {
+    let text2 = document.getElementById("texto").value;
+    let offset2 = document.getElementById("desplazamiento").value;
+    let resultadoDescifrado = cipher.decode(text2, offset2);
+    document.getElementById("textoDescifrado").innerHTML = resultadoDescifrado;
+
+}
