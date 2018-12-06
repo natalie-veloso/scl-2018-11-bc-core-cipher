2428b5c980a85d6866a4d3ef5bcd5047aa97fc46
natalie-veloso <natalie.veloso.z@gmail.com>
2018-12-06 11:55:03 -0300

    Primeros Test ok

--- cipher.js
+++ cipher.js
@@ -1,12 +1,15 @@
 window.cipher = {
   encode: (text, offset) => {
     let resultadoCifrado = "";
-       for (let i = 0; i <= text.length; i++){
+       for (let i = 0; i < text.length; i++){
           let textoEntregado = text.toUpperCase();
           let ascii = textoEntregado.charCodeAt(i)
+          if (ascii === 32){
+            resultadoCifrado += " ";}
+            else{
           let formula = (ascii - 65 + offset) % 26 + 65;
           let nuevoAscii = String.fromCharCode(formula);
-          resultadoCifrado += nuevoAscii;
+          resultadoCifrado += nuevoAscii;}
 
     }
     return resultadoCifrado;
@@ -15,12 +18,15 @@ window.cipher = {
 
   decode: (textB, offsetB) => {
     let resultadoDescifrado = "";
-        for (let i = 0; i <= textB.length; i++){
+        for (let i = 0; i < textB.length; i++){
           let textoEntregadoB = textB.toUpperCase();
           let asciiB = textoEntregadoB.charCodeAt(i)
+          if (asciiB === 32){
+            resultadoDescifrado += " ";}
+            else{
           let formulaB = ((asciiB - 90 - offsetB) % 26) + 90;
           let nuevoAsciiB = String.fromCharCode(formulaB);
-          resultadoDescifrado += nuevoAsciiB;
+          resultadoDescifrado += nuevoAsciiB;}
 
     }
     return resultadoDescifrado;
