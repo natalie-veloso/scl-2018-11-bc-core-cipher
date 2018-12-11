9c073ad03ea7b120b876423e7b5df82f815e3ef1
natalie-veloso <natalie.veloso.z@gmail.com>
2018-12-11 10:43:45 -0300

    Variables en Inglés

--- src/cipher.js
+++ src/cipher.js
@@ -1,34 +1,45 @@
 window.cipher = {
   encode: (text, offset) => {
-    let resultadoCifrado = "";
-       for (let i = 0; i < text.length; i++){
-          let textoEntregado = text.toUpperCase();
-          let ascii = textoEntregado.charCodeAt(i)
-          if (ascii === 32){
-            resultadoCifrado += " ";}
-            else{
-          let formula = (ascii - 65 + offset) % 26 + 65;
-          let nuevoAscii = String.fromCharCode(formula);
-          resultadoCifrado += nuevoAscii;}
+    let encryptedResult = "";
+    for (let i = 0; i < text.length; i++) {
+      let deliveredText = text.toUpperCase();
+      let ascii = deliveredText.charCodeAt(i)
+      if (ascii === 32) {
+        encryptedResult += " ";
+      }
+      else {
+      
+        let formula = (ascii - 65 + offset) % 26 + 65;
+        let newAscii = String.fromCharCode(formula);
+        
+        encryptedResult += newAscii;
+      }
+      /*else {
+        encryptedResult += text
+      }*/
 
     }
-    return resultadoCifrado;
+    return encryptedResult;
 
-},
+  },
 
   decode: (textB, offsetB) => {
-    let resultadoDescifrado = "";
-        for (let i = 0; i < textB.length; i++){
-          let textoEntregadoB = textB.toUpperCase();
-          let asciiB = textoEntregadoB.charCodeAt(i)
-          if (asciiB === 32){
-            resultadoDescifrado += " ";}
-            else{
-          let formulaB = ((asciiB - 90 - offsetB) % 26) + 90;
-          let nuevoAsciiB = String.fromCharCode(formulaB);
-          resultadoDescifrado += nuevoAsciiB;}
+    let decipheredResult = "";
+    for (let i = 0; i < textB.length; i++) {
+      let deliveredTextB = textB.toUpperCase();
+      let asciiB = deliveredTextB.charCodeAt(i)
+      if (asciiB === 32) {
+        decipheredResult += " ";
+      }
+      else {
+        let formulaB = ((asciiB - 90 - offsetB) % 26) + 90;
+        let newAsciiB = String.fromCharCode(formulaB);
+        
+        decipheredResult += newAsciiB;
+      }
 
     }
-    return resultadoDescifrado;
+    return decipheredResult;
 
-}};
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-window.onload=() => {
+window.onload = () => {
 
-    const cifrado =  document.getElementById("root")
-    document.getElementById("cifrar").addEventListener("click", (evento)=>{
-       evento.preventDefault();
-       let text = document.getElementById("texto").value.toUpperCase();
-       let offset = parseInt(document.getElementById("desplazamiento").value);
-
-    cifrado.innerHTML= cipher.encode(text,offset)
-   
-   });
-    
-   const descifrar =  document.getElementById("root")
-   document.getElementById("descifrar").addEventListener("click", (evento)=>{
+   const code = document.getElementById("root")
+   document.getElementById("encrypt").addEventListener("click", (evento) => {
       evento.preventDefault();
-      let text = document.getElementById("texto").value;
-      let offset = parseInt(document.getElementById("desplazamiento").value);
+      let text = document.getElementById("enterText").value.toUpperCase();
+      let offset = parseInt(document.getElementById("displacement").value);
 
-      descifrar.innerHTML= cipher.decode(text,offset)
-  
-  });
+      code.innerHTML = cipher.encode(text, offset)
 
-} 
+   });
+
+   const descifrar = document.getElementById("root")
+   document.getElementById("decipher").addEventListener("click", (evento) => {
+      evento.preventDefault();
+      let text = document.getElementById("enterText").value;
+      let offset = parseInt(document.getElementById("displacement").value);
+
+      descifrar.innerHTML = cipher.decode(text, offset)
+
+   });
+
+}
